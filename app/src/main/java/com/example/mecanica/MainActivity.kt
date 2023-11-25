e9d8373c1a7f7e8a59132240f9fa5804711491c9
Gustavodeoliveiraa <ggu3131@gmail.com>
2023-11-25 19:00:20 -0300

    logica para login e cadastro finalizada

--- app/src/main/java/com/example/mecanica/MainActivity.kt
+++ app/src/main/java/com/example/mecanica/MainActivity.kt
@@ -1,11 +1,93 @@
 package com.example.mecanica
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth;
+    private lateinit var buttonlogin: Button;
+    private lateinit var buttonregister: Button;
+    private lateinit var textlogin: EditText;
+    private lateinit var textpassword: EditText;
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth =  Firebase.auth
+
+
+        buttonlogin = findViewById<Button>(R.id.loginbutton)
+
+        buttonlogin.setOnClickListener {
+
+            textlogin = findViewById(R.id.usertext)
+            textpassword = findViewById(R.id.userpassword)
+
+            val user: String = textlogin.text.toString()
+            val password: String = textpassword.text.toString()
+
+            if(user.isNotEmpty() && password.isNotEmpty()){
+                singInWithEmailAndPassword(user, password)
+            }else{
+                Toast.makeText(this@MainActivity, "Por favor, preencha os campos", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        buttonregister = findViewById<Button>(R.id.registebutton)
+
+
+        buttonregister.setOnClickListener {
+            println("registro")
+            textlogin = findViewById(R.id.usertext)
+            textpassword = findViewById(R.id.userpassword)
+
+            val user: String = textlogin.text.toString()
+            val password: String = textpassword.text.toString()
+
+            if(user.isNotEmpty() && password.isNotEmpty()){
+                createUserWithEmailAndPassword(user, password)
+            }else{
+                Toast.makeText(this@MainActivity, "Por favor, preencha os campos", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun createUserWithEmailAndPassword(email: String, password: String){
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task->
+            if(task.isSuccessful){
+                Log.d(TAG, "createUserWithEmailAndPassword:Success")
+                val user = auth.currentUser
+                Toast.makeText(baseContext, "Register successfully", Toast.LENGTH_SHORT).show()
+
+            }else {
+                Log.w(TAG, "createUserWithEmailAndPassword:Failure", task.exception)
+                Toast.makeText(baseContext, "Authentication Failure", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun singInWithEmailAndPassword(email: String, password: String){
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task->
+            if(task.isSuccessful){
+                Log.d(TAG, "singInWithEmailAndPassword:Success")
+                val user = auth.currentUser
+            }else{
+                Log.w(TAG, "singInWithEmailAndPassword:Failure", task.exception)
+                Toast.makeText(baseContext, "Authentication Failure", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+    }
+    companion object{
+        private var TAG = "emailAndPassword"
     }
 }
