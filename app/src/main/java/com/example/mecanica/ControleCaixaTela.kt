0e813a209ca2626fcce0b9775aba216485e27487
Gustavodeoliveiraa <ggu3131@gmail.com>
2023-12-03 18:32:01 -0300

    efeito de clique nos botoes da home

---
+++ app/src/main/java/com/example/mecanica/ControleCaixaTela.kt
@@ -0,0 +1,11 @@
+package com.example.mecanica
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ControleCaixaTela : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_controle_caixa_tela)
+    }
+}
